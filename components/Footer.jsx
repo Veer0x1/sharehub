a0da5268d74577265ddfc6bf7054077da90770af
pranshu79 <pranshu.kedia.eee21@itbhu.ac.in>
2023-03-26 20:19:13 +0530

    added dashboard #9

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import Logo from "../assets/logo.png";
 const Footer = () => {
   return (
     <div>
-      <footer className="w-full bg-gray-100 dark:bg-black py-16">
+      <footer className="w-full bg-green-50 dark:bg-black py-16 mt-10">
         <div className="md:px-12 lg:px-28">
           <div className="container m-auto space-y-6 text-gray-600 dark:text-gray-300">
             <Image src={Logo} alt="logo sharehub" className="m-auto w-40" />
